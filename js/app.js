70a965511fe859b659c945f2a62aa1731f9e1c15
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-14 14:28:57 +0200

    ADD full excercise

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let km = prompt('Quanti chilometri vuoi percorrere?');
+let age = prompt('Quanti anni hai?');
+
+const pricePerKm = 0.21;
+let numberKm = parseInt(km);
+let numberAge = parseInt(age);
+
+let price = numberKm*pricePerKm;
+let discount = 0;
+let finalPrice;
+
+if(numberAge<18){
+    discount = 20;
+}
+else if(numberAge>65){
+    discount=40;
+}
+
+finalPrice = price - ((price * discount)/100);
+
+console.log(`Importo da tariffario: €${price.toFixed(2)}`);
+console.log(`Importo dovuto: €${finalPrice.toFixed(2)}`);
